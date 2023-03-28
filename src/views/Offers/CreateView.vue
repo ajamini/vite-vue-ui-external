a62ba9c11d168737325bb8fa53ab09fc5347bc83
Takinur M <takinurm@gmail.com>
2023-03-28 23:43:55 +0600

    Steps Logic

--- src/views/Offers/CreateView.vue
+++ src/views/Offers/CreateView.vue
@@ -2,12 +2,48 @@
 import { ref } from 'vue'
 
 const stepTitles = ['Basics', 'Agreement', 'Condition', 'Representation', 'Documents', 'Review']
+
+const currentStep = ref(0)
+
+const handleSubmit = () => {
+  switch (currentStep.value) {
+    case 0:
+      // Validate Form
+      // If valid, then currentStep++
+      currentStep.value = 1 //Could be currentStep.value++ but I prefer to be explicit
+      break
+    case 1:
+      // do something
+      break
+    case 2:
+      // do something
+      break
+    case 3:
+      // do something
+      break
+    case 4:
+      // do something
+      break
+    case 5:
+      // do something
+      break
+    default:
+      // do something
+      break
+  }
+}
+
+const handleGoBack = () => {
+  if (currentStep.value > 0) {
+    currentStep.value--
+  }
+}
 </script>
 
 <template>
   <div class="w-full p-8">
     Other things goes here
-    <div class="wrapper-wizard p-8 flex flex-initial bg-white mt-8 gap-2">
+    <div class="wrapper-wizard p-8 flex flex-initial bg-white shadow-xl rounded-lg mt-8 gap-2">
       <aside class="min-h-screen w-1/3 border-2 grid justify-center">
         <ul class="list-none mt-4">
           <li
@@ -24,7 +60,17 @@ const stepTitles = ['Basics', 'Agreement', 'Condition', 'Representation', 'Docum
           </li>
         </ul>
       </aside>
-      <main class="min-h-screen w-2/3 border-2 border-teal-500">Hmm</main>
+      <main class="min-h-screen w-2/3 border-2 border-teal-500">
+        <div v-if="currentStep === 0"><Basics /> Basics</div>
+        <div v-else-if="currentStep === 1"><Agreement /> Agreement</div>
+        <div v-else-if="currentStep === 2"><Condition /> Condition</div>
+        <div v-else-if="currentStep === 3"><Representation /> Representation</div>
+        <div v-else-if="currentStep === 4"><Documents /> Documents</div>
+        <div v-else-if="currentStep === 5"><Review /> Review</div>
+        <div v-else><Basics /> Basics</div>
+        <button @click="handleGoBack">Back</button>
+        <button class="ml-4" @click="handleSubmit">Next</button>
+      </main>
     </div>
   </div>
 </template>
