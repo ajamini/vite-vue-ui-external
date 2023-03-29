142704bc35a9cd5ba32468d538a365467f22eddc
Takinur M <takinurm@gmail.com>
2023-03-29 12:54:41 +0600

    Refactored Validation

--- src/views/Offers/CreateView.vue
+++ src/views/Offers/CreateView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { validateForm } from './ValidateForm'
 
 const stepTitles = ['Basics', 'Agreement', 'Condition', 'Representation', 'Documents', 'Review']
 
@@ -39,14 +40,14 @@ minDate.value.setDate(minDate.value.getDate() + 1)
 const handleSubmit = () => {
   switch (currentStep.value) {
     case 0:
-      validateForm()
+      validateForm(formData, errors)
       if (errors.value.mls !== '' || errors.value.buyers !== '') {
         return
       }
       currentStep.value = 1 //Could be currentStep.value++ but I prefer to be explicit
       break
     case 1:
-      validateForm()
+      validateForm(formData, errors)
       if (
         errors.value.offerDate !== '' ||
         errors.value.seller !== '' ||
@@ -121,55 +122,6 @@ const contactSearch = (event: any) => {
       })
     }
   }, 250)
-}
-
-const validateForm = () => {
-  //Validate form
-  if (formData.value.mls === '') {
-    errors.value.mls = 'MLS is required'
-  } else {
-    errors.value.mls = ''
-  }
-  if (formData.value.buyers.length === 0) {
-    errors.value.buyers = 'Buyers are required'
-  } else {
-    errors.value.buyers = ''
-  }
-  if (formData.value.offerDate === '') {
-    errors.value.offerDate = 'Offer Date is required'
-  } else {
-    errors.value.offerDate = ''
-  }
-  if (formData.value.seller === '') {
-    errors.value.seller = 'Seller is required'
-  } else {
-    errors.value.seller = ''
-  }
-  if (formData.value.purchasePrice === 0) {
-    errors.value.purchasePrice = 'Purchase Price is required'
-  } else {
-    errors.value.purchasePrice = ''
-  }
-  if (formData.value.depositAmount === 0) {
-    errors.value.depositAmount = 'Deposit Amount is required'
-  } else {
-    errors.value.depositAmount = ''
-  }
-  if (formData.value.depositTerms === '') {
-    errors.value.depositTerms = 'Deposit Terms is required'
-  } else {
-    errors.value.depositTerms = ''
-  }
-  if (formData.value.irrevocableDate === '') {
-    errors.value.irrevocableDate = 'Irrevocable Date is required'
-  } else {
-    errors.value.irrevocableDate = ''
-  }
-  if (formData.value.completionDate === '') {
-    errors.value.completionDate = 'Completion Date is required'
-  } else {
-    errors.value.completionDate = ''
-  }
 }
 </script>
 
