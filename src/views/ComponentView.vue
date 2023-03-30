9526e166e148aa285cfd12bf8c107d0cccb6d3ac
Takinur M <takinurm@gmail.com>
2023-03-30 13:02:02 +0600

    Payment Component

--- src/views/ComponentView.vue
+++ src/views/ComponentView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import FileUpload from '@/components/FileUpload.vue'
 import FileUploadV2 from '@/components/FileUploadV2.vue'
+import PaymentRequest from '@/components/PaymentRequest.vue'
 
 //Components are reactivated here
 </script>
@@ -14,6 +15,12 @@ import FileUploadV2 from '@/components/FileUploadV2.vue'
         <file-upload />
         <!-- component V2 -->
         <file-upload-v2 />
+      </div>
+    </section>
+    <section class="mt-4 text-center">
+      <h1 class="text-green-700 mt-4 text-2xl font-semibold">Payment Component</h1>
+      <div class="md:flex items-center gap-4 justify-center p-12">
+        <payment-request />
       </div>
     </section>
   </main>
