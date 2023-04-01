9da5d49193d02ecfe543d17034ca66e2ecde95e1
Takinur M <takinurm@gmail.com>
2023-04-01 13:03:04 +0600

    BG Styles

--- src/views/GroupsListView.vue
+++ src/views/GroupsListView.vue
@@ -317,8 +317,74 @@ const showForm = () => {
           <div class="g-card-wrapper mt-4 md:mb-4">
             <div class="bg-white shadow-lg rounded-lg">
               <div class="p-4">
-                <div class="action-wrapper flex justify-between flex-col">
-                  <h3 class="text-lg font-medium text-gray-900">{{ slotProps.data.name }}</h3>
+                <div class="action-wrapper flex justify-between">
+                  <h3 class="text-2xl font-medium text-gray-600">{{ slotProps.data.name }}</h3>
+                  <div class="flex justify-start">
+                    <button
+                      @click="viewGroup($event, slotProps.data.id)"
+                      class="flex items-center justify-center w-8 h-8 mr-0.5 text-lightblue transition-colors duration-150 cursor-pointer focus:outline-none hover:opacity-90"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-5 h-5 mt-0.5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                        />
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                      </svg>
+                    </button>
+                    <RouterLink
+                      :to="{ name: 'group-edit', params: { id: slotProps.data.id } }"
+                      class="flex items-center justify-center w-8 h-8 mr-1 text-darkblue transition-colors duration-150 cursor-pointer hover:opacity-90 focus:outline-none"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-5 h-5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                      </svg>
+                    </RouterLink>
+                    <div class="delete-wrapper">
+                      <button
+                        @click="deleteGroup($event, slotProps.data.id)"
+                        class="flex items-center justify-center w-8 h-8 mr-1 text-darkred transition-colors duration-150 cursor-pointer hover:text-gray-500 focus:outline-none"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-5 h-5"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
                 </div>
                 <div class="mt-2 p-6">
                   <h3 class="text-gray-600 text-sm font-semibold mb-4">Members</h3>
@@ -531,17 +597,11 @@ div.p-dataview-content > div {
   }
 }
 
-.p-datatable-header {
+.p-dataview-header {
   background-color: #fff !important;
   border: none !important;
 }
-.p-datatable .p-datatable-thead > tr > th {
-  background-color: #fff !important;
-}
-.p-column-title {
-  font-weight: 500;
-  color: #94a3b8;
-}
+
 ul.p-autocomplete-multiple-container {
   width: 100%;
 }
