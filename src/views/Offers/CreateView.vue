830c66c868c28a7b16df63b1ca205780a8f4019c
Takinur M <takinurm@gmail.com>
2023-04-05 01:11:52 +0600

    Review Step

--- src/views/Offers/CreateView.vue
+++ src/views/Offers/CreateView.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import ConditionAccordion from '@/components/ConditionAccordion.vue'
 import RepresentStep from '@/components/RepresentStep.vue'
+import ReviewStep from '@/components/ReviewStep.vue'
 import { ref } from 'vue'
 import { validateForm } from './ValidateForm'
 
+interface FileWithDetails extends File {
+  name: string
+}
+
 const stepTitles = ['Basics', 'Agreement', 'Condition', 'Representation', 'Documents', 'Review']
 
-const currentStep = ref(2) //Change to 0 to start from first step
+const currentStep = ref(4) //Change to 0 to start from first step - 0, 1, 2, 3, 4, 5
 const formData = ref({
-  name: '',
-  title: '',
   created_at: '',
   mls: '',
   buyers: [],
@@ -21,6 +24,8 @@ const formData = ref({
   irrevocableDate: '',
   completionDate: ''
 })
+
+const uploadedFiles = ref<FileWithDetails[]>([])
 
 //Error Message Object for Validation
 const errors = ref({
@@ -39,6 +44,7 @@ const errors = ref({
 const minDate = ref(new Date())
 minDate.value.setDate(minDate.value.getDate() + 1)
 
+// Form Submit Handler - Validate and Move to Next Step
 const handleSubmit = () => {
   switch (currentStep.value) {
     case 0:
@@ -68,23 +74,48 @@ const handleSubmit = () => {
       currentStep.value = 3
       break
     case 3:
-      // do something
+      //Move to next step
       currentStep.value = 4
       break
     case 4:
+      // Upload Files to Server
+      console.log('Upload Files', uploadedFiles.value)
       // do something
       currentStep.value = 5
       break
     case 5:
       // do something
       //Actual Submit Form Data
-      console.log('Save Data', formData.value)
+      console.log('Save these Data', formData.value)
       break
     default:
-      // do something
+      // Back to first step
       currentStep.value = 0
       break
   }
+}
+
+//File Upload Handler
+const handleFileUpload = (event: InputEvent) => {
+  const inputElement = event.target as HTMLInputElement
+  const files = inputElement.files!
+
+  for (let i = 0; i < files.length; i++) {
+    const reader = new FileReader()
+    reader.onload = () => {
+      // handle file preview if needed
+    }
+    reader.readAsDataURL(files[i])
+
+    // store the file in the uploadedFiles array
+    uploadedFiles.value.push(files[i])
+  }
+  console.log(uploadedFiles.value, 'Uploaded ')
+}
+
+//Remove file from the uploadedFiles array
+const removeFile = (index: number) => {
+  uploadedFiles.value.splice(index, 1)
 }
 
 const handleGoBack = () => {
@@ -177,7 +208,7 @@ const contactSearch = (event: any) => {
       <main class="w-full md:w-2/3">
         <form class="w-full pt-6" @submit.prevent="handleSubmit">
           <div class="steps-wrapper min-h-[240px] md:min-h-[420px]">
-            <!-- Second Step / First Step at Bottom served as Default -->
+            <!-- Second Step ~// First Step at the Bottom served as Default -->
             <div class="agree-wrapper px-8 md:flex gap-6 justify-start" v-if="currentStep === 1">
               <div class="w-1/2">
                 <div class="input-wrapper grid grid-flow-row mt-4">
@@ -280,9 +311,84 @@ const contactSearch = (event: any) => {
             <!-- Forth Step -->
             <RepresentStep :data="formData" v-else-if="currentStep === 3" />
             <!-- Fifth Step -->
-            <div v-else-if="currentStep === 4">Documents</div>
+            <div v-else-if="currentStep === 4" class="w-full md:w-3/4">
+              <h4 class="text-2xl font-semibold text-gray-700">Upload documents here</h4>
+              <div class="flex items-center justify-center mt-2">
+                <label
+                  for="dropzone-file"
+                  class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                >
+                  <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg
+                      aria-hidden="true"
+                      class="w-10 h-10 mb-3 text-gray-400"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                      ></path>
+                    </svg>
+                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                      <span class="font-semibold">Click to upload </span> or drag and drop
+                    </p>
+                    <p class="text-xs text-gray-500 dark:text-gray-400">PDF, PNG, JPG (MAX. 5MB)</p>
+                  </div>
+                  <input
+                    @change="handleFileUpload($event as InputEvent)"
+                    id="dropzone-file"
+                    type="file"
+                    class="hidden"
+                  />
+                </label>
+              </div>
+              <div class="mb-6 pt-4">
+                <div
+                  v-for="(file, index) in uploadedFiles"
+                  :key="index"
+                  class="mb-5 mt-4 rounded-md bg-[#F5F7FB] py-4 px-8"
+                >
+                  <div class="flex items-center justify-between">
+                    <span class="truncate pr-3 text-base font-medium text-[#07074D]">
+                      {{ file.name }}
+                    </span>
+                    <button @click.prevent="removeFile(index)" class="text-[#07074D]">
+                      <svg
+                        width="10"
+                        height="10"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M0.279337 0.279338C0.651787 -0.0931121 1.25565 -0.0931121 1.6281 0.279338L9.72066 8.3719C10.0931 8.74435 10.0931 9.34821 9.72066 9.72066C9.34821 10.0931 8.74435 10.0931 8.3719 9.72066L0.279337 1.6281C-0.0931125 1.25565 -0.0931125 0.651788 0.279337 0.279338Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M0.279337 9.72066C-0.0931125 9.34821 -0.0931125 8.74435 0.279337 8.3719L8.3719 0.279338C8.74435 -0.0931127 9.34821 -0.0931123 9.72066 0.279338C10.0931 0.651787 10.0931 1.25565 9.72066 1.6281L1.6281 9.72066C1.25565 10.0931 0.651787 10.0931 0.279337 9.72066Z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
             <!-- Sixth Step -->
-            <div v-else-if="currentStep === 5">Review</div>
+            <ReviewStep
+              :data="formData"
+              :files="uploadedFiles"
+              v-else-if="currentStep === 5"
+            />
             <!-- Default Step / First Step -->
             <div class="basics-wrapper px-8" v-if="currentStep === 0">
               <div class="input-wrapper grid grid-flow-row mt-4 md:w-1/2">
