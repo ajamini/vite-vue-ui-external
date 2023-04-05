04fb529c06c68a5d5bd8a6251fa4f64539220b79
Takinur M <takinurm@gmail.com>
2023-04-05 21:17:45 +0600

    Search Feature for Templates

--- src/components/offer_steps/ConditionAccordion.vue
+++ src/components/offer_steps/ConditionAccordion.vue
@@ -156,6 +156,16 @@ function handleSkip(event: Event, item_id: number) {
   })
 }
 
+const searchInput = ref('')
+const filteredTemplates = () => {
+  return conditionTemplates.value.filter((item) => {
+    return (
+      item.title.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      item.description.toLowerCase().includes(searchInput.value.toLowerCase())
+    )
+  })
+}
+
 //Add new Condition Item from Template
 function handleSelect(item_id: number) {
   console.log('Add Condition Item', item_id)
@@ -168,6 +178,7 @@ function handleSelect(item_id: number) {
       attributes: { ...item.attributes } as any //HACK: Used any to avoid type error since the interface is not matching and has dynamic attributes
     })
   }
+  showTempList.value = false //Close the modal
 }
 </script>
 
@@ -224,11 +235,40 @@ function handleSelect(item_id: number) {
           Select a template to add to the condition list. You can also edit the template to add
           additional information.
         </h3>
-        <!-- Search List -->
-        <div class="flex flex-col mt-4">Search Goes Here</div>
+        <div class="flex flex-col mt-4">
+          <div class="pt-2 relative text-gray-600">
+            <input
+              class="border-2 border-gray-300 w-full bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+              type="search"
+              name="search"
+              placeholder="Search"
+              v-model="searchInput"
+            />
+            <div class="absolute right-0 top-0 mt-5 mr-4">
+              <svg
+                class="text-gray-600 h-4 w-4 fill-current cursor-text"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 56.966 56.966"
+                style="enable-background: new 0 0 56.966 56.966"
+                xml:space="preserve"
+                width="512px"
+                height="512px"
+              >
+                <path
+                  d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
         <div class="grid grid-flow-row gap-2 pt-4">
           <div
-            v-for="(item, index) in conditionTemplates"
+            v-for="(item, index) in filteredTemplates()"
             :key="index"
             class="shadow-md rounded-sm overflow-hidden py-2 px-4 bg-gray-50"
           >
@@ -256,8 +296,6 @@ function handleSelect(item_id: number) {
               </div>
             </div>
           </div>
-          <div class="bg-white shadow-xl rounded-md overflow-hidden p-4">Ha</div>
-          <div class="bg-white shadow-xl rounded-md overflow-hidden p-4">Ha</div>
         </div>
       </div>
     </DialogModal>
