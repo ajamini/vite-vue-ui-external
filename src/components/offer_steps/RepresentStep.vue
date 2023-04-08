ec4f9935116b91ab6648a601ba7b09cd14d48187
Takinur M <takinurm@gmail.com>
2023-04-08 23:14:08 +0600

    Other Represents

--- src/components/offer_steps/RepresentStep.vue
+++ src/components/offer_steps/RepresentStep.vue
@@ -1,122 +1,7 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
 
-type Question = {
-  id: number
-  question: string
-  yes: Question | null
-  no: Question | null
-}
-
-type FinalAnswers = {
-  [key: number]: boolean
-}
-
-const props = defineProps({
-  data: {
-    type: Object,
-    required: true
-  }
-})
-
-const toHumanDate = (date: string) => {
-  const d = new Date(date)
-  return d.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })
-}
-// TODO: Replace with actual questions ~ ID should be unique currently using index
-const questions = reactive([
-  {
-    id: 1,
-    question: 'Do you know the way?',
-    yes: {
-      id: 2,
-      question: 'Have you been there?',
-      yes: {
-        id: 3,
-        question: 'Did you like it?',
-        yes: {
-          id: 4,
-          question: 'You should go there again sometime',
-          yes: null,
-          no: null
-        },
-        no: {
-          id: 5,
-          question: 'Nice try ',
-          yes: null,
-          no: null
-        }
-      },
-      no: {
-        id: 6,
-        question: 'Cool story bro',
-        yes: null,
-        no: null
-      }
-    },
-    no: {
-      id: 9,
-      question: 'You wanna know which way?',
-      yes: {
-        id: 10,
-        question: 'Are you okay with me?',
-        yes: {
-          id: 15,
-          question: 'Are you sure?',
-          yes: null,
-          no: null
-        },
-        no: {
-          id: 11,
-          question: 'okie dokie',
-          yes: null,
-          no: null
-        }
-      },
-      no: {
-        id: 12,
-        question: 'You sure?',
-        yes: {
-          id: 13,
-          question: 'Maybe you should go there',
-          yes: null,
-          no: null
-        },
-        no: {
-          id: 14,
-          question: 'Good choice',
-          yes: null,
-          no: null
-        }
-      }
-    }
-  }
-])
-
-const currentQuestion = ref<Question>(questions[0])
-
-const finalAnswers = reactive<FinalAnswers>({})
-
-function answerQuestion(answer: Question | boolean) {
-  if (answer) {
-    console.log('Howat', answer)
-    if (currentQuestion.value.yes) {
-      // Add Current Question and answer to final answers
-      Object.assign(finalAnswers, { [currentQuestion.value.question]: true })
-      currentQuestion.value = currentQuestion.value.yes
-    }
-  } else {
-    if (currentQuestion.value.no) {
-      Object.assign(finalAnswers, { [currentQuestion.value.question]: false })
-      currentQuestion.value = currentQuestion.value.no
-    } else {
-      // Object.assign(finalAnswers, { [currentQuestion.value.question]: false }) //Default to False
-      // Reset to first question
-      currentQuestion.value = questions[0]
-    }
-  }
-  console.log('Final answers', finalAnswers)
-}
+const selectedRep = ref('seller')
 </script>
 
 <template>
@@ -129,31 +14,164 @@ function answerQuestion(answer: Question | boolean) {
         <span class="text-sm text-gray-400"> Tell us who you represent </span>
       </div>
       <div class="p-4 mt-4 grid grid-rows-4 gap-6">
-        <div class="h-16 flex w-full md:w-1/2 mx-auto shadow-lg rounded bg-white">
-          <div class="flex items-center justify-center w-16 h-16 rounded-l bg-indigo-600">
-            <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
-              <path
-                fill-rule="evenodd"
-                d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-          <div class="flex flex-col text-gray-800 justify-center px-4">
-            <h3 class="text-base font-medium text-gray-800">You represnt buyer only</h3>
-          </div>
-          <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
-            <!-- SVG ICON -->
-            <svg class="w-8 h-8 -rotate-90 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
+        <div class="rep-wrapper">
+          <input
+            v-model="selectedRep"
+            class="hidden"
+            id="buyer"
+            value="buyer"
+            type="radio"
+            name="represent"
+          />
+          <label
+            for="buyer"
+            class="h-16 flex w-full md:w-1/2 mx-auto shadow-lg rounded bg-white hover:shadow-2xl cursor-pointer"
+          >
+            <div class="flex items-center justify-center w-16 h-16 rounded-l bg-indigo-600">
+              <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  fill-rule="evenodd"
+                  d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <div class="flex flex-col text-gray-800 justify-center px-4">
+              <h3 class="text-base font-medium text-gray-800">You represent buyer only</h3>
+            </div>
+            <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
+              <!-- SVG ICON -->
+              <svg
+                class="w-8 h-8 move -rotate-90 text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </label>
+        </div>
+        <div class="rep-wrapper">
+          <input
+            v-model="selectedRep"
+            class="hidden"
+            id="seller"
+            value="seller"
+            type="radio"
+            name="represent"
+          />
+          <label
+            for="seller"
+            class="h-16 flex w-full md:w-1/2 mx-auto shadow-lg rounded bg-white hover:shadow-2xl cursor-pointer"
+          >
+            <div class="flex items-center justify-center w-16 h-16 rounded-l bg-indigo-600">
+              <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  fill-rule="evenodd"
+                  d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <div class="flex flex-col text-gray-800 justify-center px-4">
+              <h3 class="text-base font-medium text-gray-800">You represent seller only</h3>
+            </div>
+            <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
+              <!-- SVG ICON -->
+              <svg class="w-8 h-8 -rotate-90 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </label>
+        </div>
+        <div class="rep-wrapper">
+          <input
+            v-model="selectedRep"
+            class="hidden"
+            id="both"
+            value="both"
+            type="radio"
+            name="represent"
+          />
+          <label
+            for="both"
+            class="h-16 flex w-full md:w-1/2 mx-auto shadow-lg rounded bg-white hover:shadow-2xl cursor-pointer"
+          >
+            <div class="flex items-center justify-center w-16 h-16 rounded-l bg-indigo-600">
+              <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  fill-rule="evenodd"
+                  d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <div class="flex flex-col text-gray-800 justify-center px-4">
+              <h3 class="text-base font-medium text-gray-800">You represent seller & buyer</h3>
+            </div>
+            <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
+              <!-- SVG ICON -->
+              <svg class="w-8 h-8 -rotate-90 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </label>
+        </div>
+        <div class="rep-wrapper">
+          <input
+            v-model="selectedRep"
+            class="hidden"
+            id="either"
+            value="either"
+            type="radio"
+            name="represent"
+          />
+          <label
+            for="either"
+            class="h-16 flex w-full md:w-1/2 mx-auto shadow-lg rounded bg-white hover:shadow-2xl cursor-pointer"
+          >
+            <div class="flex items-center justify-center w-16 h-16 rounded-l bg-indigo-600">
+              <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  fill-rule="evenodd"
+                  d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <div class="flex flex-col text-gray-800 justify-center px-4">
+              <h3 class="text-base font-medium text-gray-800">You represent either of them</h3>
+            </div>
+            <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
+              <!-- SVG ICON -->
+              <svg class="w-8 h-8 -rotate-90 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </label>
         </div>
       </div>
     </div>
   </div>
 </template>
+<style scoped>
+input:checked + label {
+  box-shadow: 0 0 0 1px #f7fafc, 0 0 0 3px #667eea;
+}
+</style>
