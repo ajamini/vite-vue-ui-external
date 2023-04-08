baaeffcad1a5c6a495bfd5e6a7fcbd1f1ae9d768
Takinur M <takinurm@gmail.com>
2023-04-08 23:22:45 +0600

    Icons

--- src/components/offer_steps/RepresentStep.vue
+++ src/components/offer_steps/RepresentStep.vue
@@ -28,19 +28,29 @@ const selectedRep = ref('seller')
             class="h-16 flex w-full md:w-1/2 mx-auto shadow-lg rounded bg-white hover:shadow-2xl cursor-pointer"
           >
             <div class="flex items-center justify-center w-16 h-16 rounded-l bg-indigo-600">
-              <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
+              <svg
+                class="w-8 h-8 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
                 <path
-                  fill-rule="evenodd"
-                  d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
-                  clip-rule="evenodd"
-                />
+                  d="m3 15 5.12-5.12A3 3 0 0 1 10.24 9H13a2 2 0 1 1 0 4h-2.5m4-.68 4.17-4.89a1.88 1.88 0 0 1 2.92 2.36l-4.2 5.94A3 3 0 0 1 14.96 17H9.83a2 2 0 0 0-1.42.59L7 19"
+                ></path>
+                <path d="m2 14 6 6"></path>
               </svg>
             </div>
             <div class="flex flex-col text-gray-800 justify-center px-4">
               <h3 class="text-base font-medium text-gray-800">You represent buyer only</h3>
             </div>
             <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
-              <!-- SVG ICON -->
+              <!-- Action ICON -->
               <svg
                 class="w-8 h-8 move -rotate-90 text-gray-400"
                 fill="currentColor"
@@ -69,19 +79,29 @@ const selectedRep = ref('seller')
             class="h-16 flex w-full md:w-1/2 mx-auto shadow-lg rounded bg-white hover:shadow-2xl cursor-pointer"
           >
             <div class="flex items-center justify-center w-16 h-16 rounded-l bg-indigo-600">
-              <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                  fill-rule="evenodd"
-                  d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
-                  clip-rule="evenodd"
-                />
+              <svg
+                class="w-8 h-8 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="m12 19 7-7 3 3-7 7-3-3z"></path>
+                <path d="m18 13-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"></path>
+                <path d="m2 2 7.586 7.586"></path>
+                <circle cx="11" cy="11" r="2"></circle>
               </svg>
             </div>
             <div class="flex flex-col text-gray-800 justify-center px-4">
               <h3 class="text-base font-medium text-gray-800">You represent seller only</h3>
             </div>
             <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
-              <!-- SVG ICON -->
+              <!-- Action ICON -->
               <svg class="w-8 h-8 -rotate-90 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                 <path
                   fill-rule="evenodd"
@@ -106,19 +126,33 @@ const selectedRep = ref('seller')
             class="h-16 flex w-full md:w-1/2 mx-auto shadow-lg rounded bg-white hover:shadow-2xl cursor-pointer"
           >
             <div class="flex items-center justify-center w-16 h-16 rounded-l bg-indigo-600">
-              <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
+              <svg
+                class="w-8 h-8 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
                 <path
-                  fill-rule="evenodd"
-                  d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
-                  clip-rule="evenodd"
-                />
+                  d="M20.42 4.58a5.4 5.4 0 0 0-7.65 0l-.77.78-.77-.78a5.4 5.4 0 0 0-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z"
+                ></path>
+                <path
+                  d="M12 5.36 8.87 8.5a2.13 2.13 0 0 0 0 3h0a2.13 2.13 0 0 0 3 0l2.26-2.21a3 3 0 0 1 4.22 0l2.4 2.4"
+                ></path>
+                <path d="m18 15-2-2"></path>
+                <path d="m15 18-2-2"></path>
               </svg>
             </div>
             <div class="flex flex-col text-gray-800 justify-center px-4">
               <h3 class="text-base font-medium text-gray-800">You represent seller & buyer</h3>
             </div>
             <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
-              <!-- SVG ICON -->
+              <!-- Action ICON -->
               <svg class="w-8 h-8 -rotate-90 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                 <path
                   fill-rule="evenodd"
@@ -155,7 +189,7 @@ const selectedRep = ref('seller')
               <h3 class="text-base font-medium text-gray-800">You represent either of them</h3>
             </div>
             <div class="flex items-center justify-center w-16 h-16 rounded-r ml-auto">
-              <!-- SVG ICON -->
+              <!-- Action ICON -->
               <svg class="w-8 h-8 -rotate-90 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                 <path
                   fill-rule="evenodd"
