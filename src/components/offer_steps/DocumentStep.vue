0567eaf9718942a8e8563d3bd690a89bb164a76a
Takinur M <takinurm@gmail.com>
2023-04-09 13:51:20 +0600

    Title and Date

--- src/components/offer_steps/DocumentStep.vue
+++ src/components/offer_steps/DocumentStep.vue
@@ -40,18 +40,26 @@ const docs = reactive([
     <div
       v-for="doc in docs"
       :key="doc.id"
-      class="w-full flex justify-start items-center px-4 border-y border-gray-200 h-28 bg-white"
+      class="w-full flex justify-start items-center px-8 border-y border-gray-200 h-28 bg-white"
       :class="{
         'border-b border-gray-200': doc.id !== docs.length,
         'border-b-0': doc.id === docs.length
       }"
     >
-      <div class="text-gray-500 text-2xl font-semibold">1</div>
+      <div class="text-gray-500 text-2xl font-semibold">{{ doc.id }}</div>
       <div class="flex ml-4 items-center">
         <div
-          class="w-7 h-7 rounded-sm bg-cyan-500 flex items-center justify-center text-white text-sm font-semibold"
+          class="w-7 h-7 uppercase rounded-sm bg-indigo-500 flex items-center justify-center text-white text-sm font-medium"
         >
-          ME
+          {{ doc.title.substring(0, 2) }}
+        </div>
+      </div>
+      <div class="flex flex-col ml-4">
+        <div class="font-sans text-gray-700 text-lg font-medium">{{ doc.title }}</div>
+        <div class="text-gray-500 text-sm font-medium">
+          <span class="text-gray-700 font-medium capitalize">{{ doc.status }}</span>
+          <span class="mx-1">•</span>
+          {{ doc.created_at }}
         </div>
       </div>
     </div>
