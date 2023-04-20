9c8cade919a8fbd2d8293d71075eccbda41d28cc
Takinur M <takinurm@gmail.com>
2023-04-20 13:01:10 +0600

    Edit Picture

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -96,6 +96,39 @@
       <div class="w-full mt-4 ml-4 rounded-md border p-4 bg-white">
         <!-- Right Side -->
         <h4 class="text-gray-500 text-xl font-bold">Edit Profile</h4>
+        <div class="w-full">
+          <div class="w-full flex justify-center">
+            <div class="relative">
+              <img
+                src="https://picsum.photos/690"
+                class="shadow-xl rounded-full align-middle border-none max-w-[80px]"
+              />
+              <!-- Edit Picture button -->
+              <div class="absolute bottom-0 right-0">
+                <label
+                  for="file-upload"
+                  class="flex items-center justify-center w-8 h-8 rounded-full bg-blue-500 cursor-pointer hover:bg-gray-200 text-white hover:text-gray-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-4 h-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                    />
+                  </svg>
+                </label>
+                <input id="file-upload" type="file" class="hidden" accept="image/*" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
