032b90f7a29a78f1bd3d4e8269bf878dd3ed8208
Takinur M <takinurm@gmail.com>
2023-04-30 11:29:44 +0600

    Responsiveness

--- src/views/Offers/DetailsView.vue
+++ src/views/Offers/DetailsView.vue
@@ -74,7 +74,6 @@ if (offer) {
           <line x1="16" x2="8" y1="12" y2="12"></line>
         </svg>
       </RouterLink>
-
       <!-- Header section -->
       <div class="w-full">
         <h1 class="text-2xl font-bold">Address, City, State, Zip</h1>
@@ -87,10 +86,11 @@ if (offer) {
         </div>
       </div>
     </div>
-    <div class="w-full">
-      <div class="flex shadow-lg pl-12 relative">
+    <!-- Tabs and Main contents -->
+    <div class="w-full relative">
+      <div class="flex shadow-lg md:pl-12 px-4">
         <div
-          class="px-6 py-2 text-sm font-medium text-gray-500 relative cursor-pointer"
+          class="px-6 py-2 text-sm font-medium text-gray-500 cursor-pointer"
           :class="{ ' text-gray-700': activeTab === 'Overview' }"
           @click="activeTab = 'Overview'"
         >
@@ -101,7 +101,7 @@ if (offer) {
           ></span>
         </div>
         <div
-          class="px-6 py-2 text-sm font-medium text-gray-500 relative cursor-pointer"
+          class="px-6 py-2 text-sm font-medium text-gray-500 cursor-pointer"
           :class="{ ' text-gray-700': activeTab === 'Agreement' }"
           @click="activeTab = 'Agreement'"
         >
@@ -112,7 +112,7 @@ if (offer) {
           ></span>
         </div>
         <div
-          class="px-6 py-2 text-sm font-medium text-gray-500 relative cursor-pointer"
+          class="px-6 py-2 text-sm font-medium text-gray-500 cursor-pointer"
           :class="{ ' text-gray-700': activeTab === 'Represent' }"
           @click="activeTab = 'Represent'"
         >
@@ -122,60 +122,60 @@ if (offer) {
             class="absolute shadow-md border-b-2 border-b-red-500 left-1/3 bottom-0 w-1/3"
           ></span>
         </div>
-        <div class="absolute right-1/4 top-4">
-          <!-- Rounded button for copy link -->
-          <button class="rounded-full shadow bg-white p-2 text-gray-700 hover:bg-gray-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-5 h-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-              />
-            </svg>
-          </button>
-          <button class="mx-4 rounded-full bg-white shadow p-2 text-gray-700 hover:bg-gray-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-5 h-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-              />
-            </svg>
-          </button>
-          <button
-            class="inline-flex overflow-hidden px-4 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br rounded-full shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
+      </div>
+      <!-- Buttons for Actions -->
+      <div class="relative md:absolute md:right-1/4 md:top-4 p-4 md:p-0">
+        <button class="rounded-full shadow bg-white p-2 text-gray-700 hover:bg-gray-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-5 h-5 mr-2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z"
-              />
-            </svg>
-            <span>End Print</span>
-          </button>
-        </div>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+            />
+          </svg>
+        </button>
+        <button class="mx-4 rounded-full bg-white shadow p-2 text-gray-700 hover:bg-gray-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+            />
+          </svg>
+        </button>
+        <button
+          class="inline-flex overflow-hidden px-4 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br rounded-full shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5 mr-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z"
+            />
+          </svg>
+          <span>End Print</span>
+        </button>
       </div>
       <!-- Tab Contents -->
       <div class="w-full mt-4">
